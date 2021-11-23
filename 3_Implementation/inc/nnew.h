f2bcff4dab329b03c258d6ef6466b53eb16c5eb9
Naralasetty kesava kumar <94446387+Kesava435@users.noreply.github.com>
2021-11-23 14:40:00 +0530

    Add files via upload

---
+++ 3_Implementation/inc/nnew.h
@@ -0,0 +1,12 @@
+#ifndef __nnew_H__
+#define __nnew_H__
+
+
+typedef char bit;
+bit and_and(bit a,bit b);
+bit or_ro(bit c,bit d);
+
+bit nt(bit e);
+bit nand(bit a,bit b);
+bit nor(bit a,bit b);
+#endif
